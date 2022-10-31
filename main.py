e02b5e3d5f33420754f72b9728eb8b755aa03d7d
manishtmtmt <manisht.mt.mt@gmail.com>
2022-10-31 10:28:56 +0530

    opened the site and write fun for append to json

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import json
 from selenium import webdriver
 from time import sleep
 
-
+# Opening the website on Chrome
 driver = webdriver.Chrome()
 driver.get("https://food.grab.com/sg/en/restaurants")
 
 driver.maximize_window()
+
+# Function for append data to json file
+def write_json(new_data, filename='restaurants.json'):
+    with open(filename, 'r+') as file:
+        file_data = json.load(file)
+        file_data['restaurants'].append(new_data)
+        file.seek(0)
+        json.dump(file_data, file, indent=4)
+
