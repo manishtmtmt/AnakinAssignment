b381ad3e14238786188a027b2337d852fb15acf2
manishtmtmt <manisht.mt.mt@gmail.com>
2022-10-31 10:32:27 +0530

    got restaurants data, add lat, lan to json file

--- main.py
+++ main.py
@@ -9,10 +9,37 @@ driver.get("https://food.grab.com/sg/en/restaurants")
 driver.maximize_window()
 
 # Function for append data to json file
-def write_json(new_data, filename='restaurants.json'):
-    with open(filename, 'r+') as file:
+def write_json(new_data, filename="restaurants.json"):
+    with open(filename, "r+") as file:
         file_data = json.load(file)
-        file_data['restaurants'].append(new_data)
+        file_data["restaurants"].append(new_data)
         file.seek(0)
         json.dump(file_data, file, indent=4)
 
+
+for i in range(15):
+    # get the data from website
+    data = driver.find_element("id", "__NEXT_DATA__").get_attribute("innerHTML")
+    parsedData = json.loads(data)
+
+    restaurantLists = parsedData["props"]["initialReduxState"]["pageRestaurantsV2"][
+        "entities"
+    ]["restaurantList"]
+
+    # append restaurant to json file
+    for key in restaurantLists:
+        restaurant = {}
+        restaurant["title"] = restaurantLists[key]["name"]
+        restaurant["latitude"] = restaurantLists[key]["latitude"]
+        restaurant["longitude"] = restaurantLists[key]["longitude"]
+        write_json(restaurant)
+
+    # catch the load more button and clicking it
+    load_more = driver.find_element(
+        "xpath", '//*[@id="page-content"]/div[4]/div/div/div[4]/div/button'
+    )
+    load_more.click()
+    sleep(10)
+
+
+driver.quit()
